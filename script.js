5e88bb452c06d707e80effdaa472b94958704214
youssefskaw <121441259+YOUSSEFWAEL54@users.noreply.github.com>
2024-09-02 02:12:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // دالة للتنقل بين الأقسام
+
   function showSection(targetSection) {
-    // إخفاء جميع الأقسام
     const sections = document.querySelectorAll('.MainSection > div');
     sections.forEach(section => {
       section.style.display = 'none';
     });
 
-    // عرض القسم المستهدف
     document.querySelector(targetSection).style.display = 'block';
   }
 
-  // عند الضغط على زر "My Skills"
   document.querySelector('.about_me .blue-btn').addEventListener('click', function() {
+    showSection('.who_me');
+  });
+
+  document.querySelector('.who_me .blue-btn').addEventListener('click', function() {
     showSection('.skills');
   });
 
-  // عند الضغط على زر "Contact Me"
+
   document.querySelector('.skills .blue-btn').addEventListener('click', function() {
     showSection('.contact');
   });
 
-  // عرض القسم الأول بشكل افتراضي
   showSection('.about_me');
 });
